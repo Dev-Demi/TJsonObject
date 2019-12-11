5b661fcec0a106a29b711fa439ab8227f5c40d3e
Dev-Demi <BoxDemi@gmail.com>
2019-12-11 15:40:11 +0300

    Start
    Serialize, deserialize json object. Delphi

---
+++ UJsonObject.pas
@@ -0,0 +1,130 @@
+unit UJsonObject;
+
+interface
+uses
+ System.JSON.Serializers,System.JSON.Types,Generics.Collections;
+type
+ TJsonObjectEnumerator<T> = class;
+{$M+}
+  TJsonObject<T> = class(TObject)
+  private
+    fMyObjectList:TList<TJsonObject<T>>;
+    fNode:T;
+  public
+   Constructor Create;
+   Destructor Free;
+   Function SaveToText:String;
+   Procedure LoadFromText(JsonText:string);
+   Function ForInStorage:TJsonObjectEnumerator<T>;
+  published
+    property Node:T read fNode write fNode;
+    property Storage:TList<TJsonObject<T>> read fMyObjectList write fMyObjectList;
+  end;
+{$M-}
+
+ TJsonObjectEnumerator<T>=class
+    private
+     ForInIndex:integer;
+     fEnumItem:TJsonObject<T>;
+     function GetCurrentItem:TJsonObject<T>;
+    public
+    // обслуживание перечислителя, не для внешнего использования!
+    property Current:TJsonObject<T> read GetCurrentItem;
+    function MoveNext:boolean;
+    function GetEnumerator:TJsonObjectEnumerator<T>;{$IFDEF SUPPORTS_INLINE}inline;{$ENDIF}
+    // генерировать перечень файлов
+    Constructor Create(EnumItem:TJsonObject<T>);
+    Destructor Destroy;override;
+  end;
+
+implementation
+ uses SysUtils;
+
+{ TJsonObject<T> }
+
+constructor TJsonObject<T>.Create;
+begin
+ Storage:=TList<TJsonObject<T>>.Create;
+end;
+
+function TJsonObject<T>.ForInStorage: TJsonObjectEnumerator<T>;
+begin
+ result:= TJsonObjectEnumerator<t>.Create(self);
+end;
+
+destructor TJsonObject<T>.Free;
+var
+ tmp:T;
+ tmpJsonObj: TJsonObject<T>;
+begin
+ tmp:=Node;
+ FreeAndNil(tmp);
+ while Storage.Count>0 do
+ begin
+  tmpJsonObj:=storage[0];
+  storage.Remove(tmpJsonObj);
+  tmpJsonObj.Free;
+ end;
+ Storage.Free;
+end;
+
+procedure TJsonObject<T>.LoadFromText(JsonText: string);
+var
+ LSerializer : TJsonSerializer;
+begin
+ LSerializer := TJsonSerializer.Create;
+ LSerializer.Formatting := TJsonFormatting.Indented;
+ LSerializer.Populate(JsonText,self);
+ LSerializer.Free
+end;
+
+function TJsonObject<T>.SaveToText: String;
+var
+ LSerializer : TJsonSerializer;
+begin
+ LSerializer := TJsonSerializer.Create;
+ LSerializer.Formatting := TJsonFormatting.Indented;
+ result:= LSerializer.Serialize(self);
+ LSerializer.Free;
+end;
+
+{ TJsonObjectEnumerator<T> }
+
+constructor TJsonObjectEnumerator<T>.Create(EnumItem: TJsonObject<T>);
+begin
+ fEnumItem:= EnumItem;
+ ForInIndex:=-1;
+end;
+
+destructor TJsonObjectEnumerator<T>.Destroy;
+begin
+
+  inherited;
+end;
+
+function TJsonObjectEnumerator<T>.GetCurrentItem: TJsonObject<T>;
+begin
+ result:=nil;
+
+ if ForInIndex<0 then exit;
+  if fEnumItem.storage.Count>0 then
+   if ForInIndex<fEnumItem.storage.Count then
+    result:= fEnumItem.Storage[ForInIndex];
+end;
+
+function TJsonObjectEnumerator<T>.GetEnumerator: TJsonObjectEnumerator<T>;
+begin
+ result:=self;
+end;
+
+function TJsonObjectEnumerator<T>.MoveNext: boolean;
+begin
+ result:=true;
+ if ForInIndex>=fEnumItem.storage.Count-1 then
+  result:=false;
+
+ if result then
+  ForInIndex:=ForInIndex+1;
+end;
+
+end.
---
+++ Unit1.pas
@@ -0,0 +1,107 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,UJsonObject;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+{$M+}
+  TMyTest = class(TObject)
+  private
+    fInt:Integer;
+    fString:String;
+  public
+   Constructor Create;
+   Destructor Free;
+  published
+    property MyStr: string read fString write fString;
+    property MyInt: Integer read fInt write fInt;
+  end;
+{$M-}
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ i:integer;
+ SObj:TJsonObject<TMyTest>;
+ c:TJsonObject<TMyTest>;
+ MyNodeTest:TMyTest;
+begin
+ SObj:=TJsonObject<TMyTest>.Create;
+ // Create data object   // Создаем объект, который хотим сохранить
+  MyNodeTest:=TMyTest.Create;
+  MyNodeTest.MyStr:='Test JSON serialize, тест';
+  MyNodeTest.MyInt:=100;
+ // Connect data object to Storage Object // Указываем ссылку на него
+ SObj.Node:=MyNodeTest;
+ // Create SubObject    // Создаем подобъекты (древовидная структура)
+ for i:=1 to 10 do
+  begin
+    c:=TJsonObject<TMyTest>.Create;
+     MyNodeTest:=TMyTest.Create;
+     MyNodeTest.MyStr:='Str'+inttostr(i);
+     MyNodeTest.MyInt:=i;
+    c.Node:=MyNodeTest;
+    SObj.Storage.Add(c);
+  end;
+
+ Memo1.Lines.Text:= SObj.SaveToText;
+ SObj.Free;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+ SObj,item:TJsonObject<TMyTest>;
+begin
+ SObj:=TJsonObject<TMyTest>.Create;
+ SObj.LoadFromText(Memo1.Lines.Text);
+ Memo1.Lines.Add(SObj.Node.MyStr);
+
+ // Classic method
+ // for i:=0 to SObj.Storage.Count-1 do
+  // Memo1.Lines.Add(SObj.Storage[i].Node.MyStr);
+
+ // ForIn method
+ for item in SObj.ForInStorage do
+  Memo1.Lines.Add(item.Node.MyStr);
+
+ SObj.Free;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+ ReportMemoryLeaksOnShutdown := True;
+end;
+
+{ TMyTest }
+
+constructor TMyTest.Create;
+begin
+
+end;
+
+destructor TMyTest.Free;
+begin
+
+end;
+
+end.
